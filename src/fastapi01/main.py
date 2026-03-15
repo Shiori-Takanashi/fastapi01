080f756e0888da0c3c4108c962fc7036d73c415a
Takanashi <nao69alter@gmail.com>
2026-03-15 09:12:59 +0900

    fix: configure_loggingの返り値Noneに合わせて仕様変更

--- src/fastapi01/main.py
+++ src/fastapi01/main.py
@@ -1,12 +1,9 @@
-import logging
 from fastapi import FastAPI
 
 from fastapi01.presentation.routes import router
-from fastapi01.logconfig.logger import configure_logging
+from fastapi01.logconfig.loggers import configure_logging
 
-logger_name = "fastapi01"
-logger = logging.getLogger(logger_name)
-configure_logging(logger_name)
+logger = configure_logging(__name__)
 
 app = FastAPI()
 
